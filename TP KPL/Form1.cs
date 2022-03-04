1349ebe6f081bccde170592ee7ad517b8c26ac7e
Sumryooo <100459775+Sumryooo@users.noreply.github.com>
2022-03-04 15:57:09 +0700

    Add files via upload

---
+++ TP KPL/Form1.cs
@@ -0,0 +1,20 @@
+namespace TP_KPL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text;
+        }
+    }
+}
